cf9640d5f9b9798e3b02ead880dde187fd04e0d2
nayakom16 <nayakankush71@gmail.com>
2025-05-16 15:16:38 +0530

    PORTFOLIO

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
 
                             >
                                 <motion.img src={item.img} alt=""
-                                    initial={{ opacity: 0, x: -500 }}
+                                    initial={{ opacity: 0, x: -100 }}
                                     whileInView={{ opacity: 1, x: 0 }}
                                     transition={{ duration: 1, ease: 'easeOut' }}
                                 />
